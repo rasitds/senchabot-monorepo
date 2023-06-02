df0e2b5dc01407746de00f53c3fcce4d32e042ca
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-06-03 02:53:26 +0300

    refactor(apps/twitch-bot): make handlers and webhooks structs private

--- apps/twitch-bot/internal/handler/handler.go
+++ apps/twitch-bot/internal/handler/handler.go
@@ -12,25 +12,25 @@ type Handler interface {
 	InitHttpHandlers(mux *http.ServeMux)
 }
 
-type Handlers struct {
+type handlers struct {
 	joinedChannelList []string
 	client            *client.Clients
 	service           *service.Services
 }
 
-func (s *Handlers) InitBotEventHandlers() {
+func (s *handlers) InitBotEventHandlers() {
 	PrivateMessage(s.client, s.service)
 	s.joinedChannelList = BotJoin(s.client, s.service)
 }
 
-func (s *Handlers) InitHttpHandlers(mux *http.ServeMux) {
+func (s *handlers) InitHttpHandlers(mux *http.ServeMux) {
 	mux.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
 		s.service.Webhook.BotJoin(s.client, s.joinedChannelList, w, r)
 	})
 }
 
 func NewHandlers(client *client.Clients, service *service.Services) Handler {
-	return &Handlers{
+	return &handlers{
 		client:  client,
 		service: service,
 	}
